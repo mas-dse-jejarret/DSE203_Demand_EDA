aeb5a419bad20b2e471142c83b558708de97d70c
toby <toby.moreno@gmail.com>
2017-12-07 22:19:49 -0800

    added optimization/caching

--- www/rest_api_server.py
+++ www/rest_api_server.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 from flask import Flask, request, jsonify
 from data_exploration2 import *
+from werkzeug.contrib.cache import SimpleCache
 
-import json
+cache = SimpleCache()
 
 app = Flask(__name__)
 
@@ -12,9 +13,15 @@ months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 def root():
     return "Welcome to Data Exploration API!"
 
+@app.route("/clearCache")
+def clearCache():
+    cache.clear()
+    return "cleared"
+
 @app.route("/api/highest_monthly_sales_by_category/<list>/<limit>")
 def api_highest_monthly_sales_by_category(list, limit):
     category_list = list.split(",")
+
     hmsb = HighestMonthlySalesByCategory(category_list, limit)
 
     # print(hmsb)
@@ -27,17 +34,27 @@ def api_highest_monthly_sales_by_category(list, limit):
 
 @app.route("/api/top_sales_category/<ncategories>/<list>")
 def api_top_sales_category(ncategories, list):
+    key = "{0}:{1}".format(ncategories, list)
+    tc = cache.get(key)
+    if tc is None:
+        month_list = [int(x) for x in list.split(",")]
+
+        print(month_list)
+        print(ncategories)
+
+        tc = OptimizedTopCategories(int(ncategories), month_list)
+        cache.set(key, tc, timeout=60 * 2)
+
+    return (jsonify([{"name": item[0], "value": item[1]} for item in tc]))
+
+@app.route("/api/top_sales_category2/<ncategories>/<list>")
+def api_top_sales_category2(ncategories, list):
     month_list = [int(x) for x in list.split(",")]
 
     print(month_list)
     print(ncategories)
 
     tc = OptimizedTopCategories(int(ncategories), month_list)
-
-    print(tc)
-
-    # for item in tc:
-    #     print(months[item['mon'] - 1])
 
     return (jsonify([{"name": item[0], "value": item[1]} for item in tc]))
 
